d89c4de997f9cde41eb0f74f0d4bedc182c3f6be
jeremymiller17 <jeremy.miller1@snhu.edu>
2022-10-09 22:46:14 -0400

    Added app.py/ inline comments

--- SecurePass.py
+++ SecurePass.py
@@ -2,28 +2,21 @@ import string
 import secrets
 
 
-class SecurePass:
-    @staticmethod
-    def create_password(length: int, symbols: bool, uppercase: bool):
-        # Concatenates  into a unified string based on parameter values
-        combination = string.ascii_lowercase + string.digits
+def create_password(length: int, symbols: bool, uppercase: bool):
+    # Concatenates  into a unified string based on parameter values
+    combination = string.ascii_lowercase + string.digits
 
-        if symbols:
-            combination += string.punctuation
+    if symbols:
+        combination += string.punctuation
 
-        if uppercase:
-            combination += string.ascii_uppercase
+    if uppercase:
+        combination += string.ascii_uppercase
 
-        combination_length = len(combination)
+    combination_length = len(combination)
 
-        gen_password = ""
+    gen_password = ""
 
-        for _ in range(length):
-            gen_password += combination[(secrets.randbelow(combination_length))]
+    for _ in range(length):
+        gen_password += combination[(secrets.randbelow(combination_length))]
 
-        return gen_password
-
-
-generator = SecurePass()
-# Test creates a password that is 20 chars long, with symbols and upper/lowercase letters
-#print(generator.create_password(length=20, symbols=True, uppercase=True))
+    return gen_password
---
+++ app.py
@@ -0,0 +1,25 @@
+import SecurePass as sp
+import InjectPassword as ip
+
+"""CAUTION: Saving a password anywhere is compromising. However, if you need to do it make it a tiny bit harder to spot.
+Once your .jpg of choice is injected move it out of the programs directory to a secure/secret location.
+"""
+def main():
+    # images/ was created to keep repo clean. hide_secret/find_hidden can be ran with .jpgs in root directory
+    base_image = 'images/palletcleanser.jpg' # image to be changed
+    output_image = 'output.jpg' # The password injected image.
+
+    """NOTE: new_password is generated based off of 3 parameters, length, symbols, and uppercase.
+    For strongest results generate a password with a length that is the maximum the platform allows,
+    use symbols and add uppercase letters."""
+    new_password = sp.create_password(length=20, symbols=True, uppercase=True)
+    print(new_password)
+    # baseimage is copied then injected with new_password in the root directory
+    ip.hide_secret(base_image, output_image, new_password)
+
+    # find_hidden reads the bytes of the .jpg file and looks for hidden text.
+    ip.find_hidden('output.jpg')
+
+
+if __name__ == "__main__":
+    main()
